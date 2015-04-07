40d0fe0671c7d98a83e47ecc7a5554797d7b5651
ANDRES KWAN <andreskwan@gmail.com>
2015-04-07 06:07:43 -0500

    CC.12 - remove code to allow to use the storyboard

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -18,42 +18,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Label
-        //1 instantiate label
-        var label = UILabel()
-        //2 create frame for label
-        label.frame = CGRectMake(130, 150, 60, 60)
-        //3 set label text
-        label.text = "0"
-        //4 add subview
-        self.view.addSubview(label)
-        //5 associate attribute to the UILabel
-        self.label = label
-        
-        //Label
-        var labelIncrement = UILabel()
-        labelIncrement.frame = CGRectMake(260, 150, 60, 60)
-        labelIncrement.text = "0"
-        self.view.addSubview(labelIncrement)
-        self.labelIncrement = labelIncrement
-        
-        //Button
-        var button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
-        button.setTitleColor(UIColor.blackColor(), forState: .Normal)
-        self.view.addSubview(button)
-        //add button target action pattern
-        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
-        
-        //Decrement Button
-        var decrementButton = UIButton()
-        decrementButton.frame = CGRectMake(150, 320, 60, 60)
-        decrementButton.setTitle("Decrement", forState: .Normal)
-        decrementButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
-        self.view.addSubview(decrementButton)
-        //add button target action pattern
-        decrementButton.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
     }
     func incrementCount(){
         self.count++
